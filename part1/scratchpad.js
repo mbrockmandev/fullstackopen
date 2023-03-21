a41f6590429d4ef34b90f5d6e28ef45e93109f36
Michael Brockman <mbrockmandev@gmail.com>
2023-03-21 14:46:56 -0400

    add obj work

---
+++ part1/scratchpad.js
@@ -0,0 +1,23 @@
+const arto = {
+  name: 'Arto Hellas',
+  age: 35,
+  education: 'PhD',
+  greet: function () {
+    console.log('hello, my name is ' + this.name);
+  },
+  doAddition: function (a, b) {
+    console.log(a + b);
+  },
+};
+
+arto.growOlder = function () {
+  this.age += 1;
+};
+
+console.log(arto.age); // 35 is printed
+arto.growOlder();
+console.log(arto.age); // 36 is printed
+
+arto.doAddition(1, 4); // 5 is printed
+const referenceToAddition = arto.doAddition;
+referenceToAddition(10, 15); // 25 is printed
