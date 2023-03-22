4d444a08dd286ce3b63ddada3633768339d43bd6
Michael Brockman <mbrockmandev@gmail.com>
2023-03-22 19:17:32 -0400

    fin 2.11

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,65 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Note from './components/Note';
+import Filter from './components/Filter';
+import Persons from './components/Persons';
+import PersonForm from './components/PersonForm';
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState('');
-  const [showAll, setShowAll] = useState(false);
+  const [persons, setPersons] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    console.log('effect');
-    axios.get('http://localhost:3001/notes').then((response) => {
-      console.log('promise fulfilled');
-      setNotes(response.data);
-    });
-  }, []);
-
-  console.log('render', notes.length, 'notes');
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
-    };
-
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => setPersons(response.data));
   };
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value);
+  useEffect(hook, []);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   };
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase()),
+  );
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
-      <ul>
-        <ul>
-          {notesToShow.map((note) => (
-            <Note
-              key={note.id}
-              note={note}
-            />
-          ))}
-        </ul>
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type='submit'>save</button>
-      </form>
+      <h2>Phonebook</h2>
+      <Filter
+        persons={persons}
+        handleFilter={handleFilterChange}
+      />
+      <h3>Add a new</h3>
+      <PersonForm />
+      <h2>Numbers</h2>
+      <Persons persons={filteredPersons} />
     </div>
   );
 };
