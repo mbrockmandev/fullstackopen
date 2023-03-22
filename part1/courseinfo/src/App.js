6ba0406df21d32810eefcbb72ee473b6fbe53c4a
Michael Brockman <mbrockmandev@gmail.com>
2023-03-21 20:08:08 -0400

    add history component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,38 +1,35 @@
 import { useState } from 'react';
-import Display from './components/Display';
-import Button from './components/Button';
+import History from './components/History';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
 
-  const increaseByOne = (e) => {
-    setCounter(counter + 1);
-  };
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
 
-  const resetToZero = (e) => {
-    setCounter(0);
-  };
 
-  const decreaseByOne = (e) => {
-    setCounter(counter - 1);
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + right) 
   };
 
   return (
-    <>
-      <Display counter={counter} />
-      <Button
-        handleClick={increaseByOne}
-        text='Plus'
-      />
-      <Button
-        handleClick={resetToZero}
-        text='Zero'
-      />
-      <Button
-        handleClick={decreaseByOne}
-        text='Minus'
-      />
-    </>
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+    </div>
   );
 };
 
