b656f46c7e1dbace1ecdc540b0500f8b482b93da
Michael Brockman <mbrockmandev@gmail.com>
2023-03-22 14:16:44 -0400

    add 2.9

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,13 +3,17 @@ import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
+  const [filteredPeople, setFilteredPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const name = e.target.querySelector('#name');
-    const phone = e.target.querySelector('#phone');
+    const number = e.target.querySelector('#number');
     const result = persons.some((item) => item.name.includes(name.value));
     if (result) {
       alert(
@@ -18,40 +22,67 @@ const App = () => {
       return;
     }
 
-    setPersons(persons.concat({ name: name.value, phone: phone.value }));
+    setPersons(persons.concat({ name: name.value, number: number.value }));
     name.value = '';
-    phone.value = '';
+    number.value = '';
+  };
+
+  const handleFilter = (e) => {
+    const nameInput = e.target.value.toLowerCase();
+    console.log('input:', nameInput);
+    const filtered = persons.filter((person) => {
+      return person.name.toLowerCase().includes(nameInput);
+    });
+
+    setFilteredPeople(filtered);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input id='filter' />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
           name:
           <input
             id='name'
             placeholder='Enter a name.'
+            onChange={handleFilter}
           />
         </div>
         <div>
           <div>
-            number: <input id='phone' />
+            number: <input id='number' />
           </div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => {
-          const id = uuidv4();
-          return (
-            <li key={id}>
-              {person.name} - {person.phone}
-            </li>
-          );
-        })}
-      </ul>
+      {filteredPeople.length > 0 ? (
+        <ul>
+          {filteredPeople.map((person) => {
+            const id = uuidv4();
+            return (
+              <li key={id}>
+                {person.name} - {person.number}
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <ul>
+          {persons.map((person) => {
+            const id = uuidv4();
+            return (
+              <li key={id}>
+                {person.name} - {person.number}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
