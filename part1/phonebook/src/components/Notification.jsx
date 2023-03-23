5e130bc11dc70a2f71af90f666446d2e0112b55e
Michael Brockman <mbrockmandev@gmail.com>
2023-03-23 14:43:37 -0400

    fin 2.17*

--- part1/phonebook/src/components/Notification.jsx
+++ part1/phonebook/src/components/Notification.jsx
@@ -1,8 +1,6 @@
 const Notification = ({ message, type }) => {
   if (message === null) return null;
 
-  console.log('notificationType:', type);
-
   return <div className={type}>{message}</div>;
 };
 
