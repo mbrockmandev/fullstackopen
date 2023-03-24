b40b0009e9cb2960d466fa873e3e0a0db6582edf
Michael Brockman <mbrockmandev@gmail.com>
2023-03-24 10:10:38 -0400

    fin thru 3.6

---
+++ part3/notes-backend/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
+];
+
+// GET TEST
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+// GET ALL NOTES
+app.get('/api/notes/', (req, res) => {
+  res.json(notes);
+});
+
+// GET BY ID
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+    return note.id === id;
+  });
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+// Helper to make new ID
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+// POST NEW NOTE
+app.post('/api/notes', (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(404).json({ error: 'Content Missing.' });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  res.json(note);
+});
+
+// DELETE BY ID
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/info/', (req, res) => {
+  const now = new Date();
+  const formatted = now.getDate();
+
+  const responseHtml = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Info:</title>
+    </head>
+    <body>
+      <h3>There are ${persons.length} contacts in the phonebook.</h3>
+      <h3>The current time is: ${formatted}</h3>
+    </body>
+  </html>
+`;
+  res.status(200).send(responseHtml);
+});
+
+// GET ALL persons
+app.get('/api/persons/', (req, res) => {
+  res.json(persons);
+});
+
+// GET BY ID
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => {
+    console.log(person.id, typeof person.id, id, typeof id, person.id === id);
+    return person.id === id;
+  });
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+// Helper to make new ID
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+  return maxId + 1;
+};
+
+// helper to find duplicate names
+const isDuplicate = (name) => {
+  return persons.map((person) => person.name).includes(name);
+};
+
+// POST NEW person
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  // Validate input
+  if (!body.name) {
+    return res.status(404).json({ error: 'Name Missing.' });
+  } else if (!body.number) {
+    return res.status(404).json({ error: 'Number Missing.' });
+  } else if (isDuplicate(body.name)) {
+    return res.status(404).json({ error: 'Duplicate Name.' });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number || false,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+// DELETE BY ID
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
