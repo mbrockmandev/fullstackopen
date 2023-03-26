be27b97f23e2c1c6e0d6f706fa986b2420d6cb09
Michael Brockman <mbrockmandev@gmail.com>
2023-03-26 11:30:18 -0400

    done through 4.7*

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -5,4 +5,59 @@ const dummy = (blogs) => {
   return totalLikes;
 };
 
-module.exports = { dummy };
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return 0;
+
+  const max = blogs.reduce((prev, curr) =>
+    prev.likes > curr.likes ? prev : curr,
+  );
+
+  return max;
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return {};
+  const authors = blogs.reduce((acc, curr) => {
+    const index = acc.findIndex((item) => item.author === curr.author);
+    if (index > -1) {
+      acc[index].blogs++;
+    } else {
+      acc.push({ author: curr.author, blogs: 1 });
+    }
+    return acc;
+  }, []);
+
+  const mostProlificAuthor = authors.reduce((max, author) =>
+    max.blogs > author.blogs ? max : author,
+  );
+
+  return mostProlificAuthor;
+};
+
+const mostLiked = (blogs) => {
+  if (blogs.length === 0) return {};
+
+  const authors = blogs.reduce((acc, curr) => {
+    const index = acc.findIndex((item) => item.author === curr.author);
+    if (index > -1) {
+      acc[index].likes += curr.likes;
+    } else {
+      acc.push({ author: curr.author, likes: curr.likes });
+    }
+    return acc;
+  }, []);
+
+  const mostLikedAuthor = authors.reduce((max, author) =>
+    max.likes > author.likes ? max : author,
+  );
+
+  console.log(mostLikedAuthor);
+  return mostLikedAuthor;
+};
+
+module.exports = {
+  dummy,
+  favoriteBlog,
+  mostBlogs,
+  mostLiked,
+};
