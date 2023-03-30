36f6cb6dcb7969a487a36e5b3c3d52e13633dafb
Michael Brockman <mbrockmandev@gmail.com>
2023-03-30 14:13:43 -0400

    done w 5.4 - add notifications to blog

--- part5/blog/frontend/src/App.js
+++ part5/blog/frontend/src/App.js
@@ -1,15 +1,29 @@
 import { useState, useEffect } from 'react';
+import './App.css';
 import Blog from './components/Blog';
+import AddBlogForm from './components/AddBlogForm';
 import LoginForm from './components/LoginForm';
 import blogService from './services/blogs';
 import loginService from './services/login';
+import Notification from './components/Notification';
 
 const App = () => {
+  // list of blogs
   const [blogs, setBlogs] = useState([]);
 
+  // user login
   const [username, setUsername] = useState('mike');
   const [password, setPassword] = useState('12345');
   const [user, setUser] = useState(null);
+
+  // new blog entry
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
+
+  // notifications
+  const [notificationMessage, setNotificationMessage] = useState(null);
+  const [notificationType, setNotificationType] = useState(null);
 
   useEffect(() => {
     const loggedInUserJSON = window.localStorage.getItem('blogAppUser');
@@ -21,16 +35,43 @@ const App = () => {
     }
   }, []);
 
+  const handleAddBlog = async (e) => {
+    e.preventDefault();
+    try {
+      const newBlog = {
+        title,
+        author,
+        url,
+      };
+      console.log('newBlog:', newBlog);
+      await blogService.create(newBlog);
+      setBlogs([...blogs, newBlog]);
+      const message = `Successfully added "${newBlog.title}!"`;
+      showNotification(message, 'success');
+      setAuthor('');
+      setTitle('');
+      setUrl('');
+    } catch (error) {
+      console.error(error.message);
+      const message = `Error: ${error.message}!`;
+      showNotification(message, 'error');
+    }
+  };
+
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const user = await loginService.login({ username, password });
       window.localStorage.setItem('blogAppUser', JSON.stringify(user));
       blogService.setToken(user.token);
+      const message = `${user.username} logged in!"`;
+      showNotification(message, 'success');
       setUser(user);
       setUsername('');
       setPassword('');
     } catch (error) {
+      const message = 'Incorrect username/password.';
+      showNotification(message, 'error');
       console.log(error);
     }
   };
@@ -41,28 +82,60 @@ const App = () => {
     window.localStorage.removeItem('blogAppUser');
   };
 
+  const showNotification = (message, type, duration = 5000) => {
+    setNotificationMessage(message);
+    setNotificationType(type);
+
+    setTimeout(() => {
+      setNotificationMessage(null);
+      setNotificationType(null);
+    }, duration);
+  };
+
   if (!user) {
     return (
-      <LoginForm
-        username={username}
-        setUsername={setUsername}
-        password={password}
-        setPassword={setPassword}
-        handleLogin={handleLogin}
-      />
+      <>
+        <Notification
+          message={notificationMessage}
+          type={notificationType}
+        />
+        <LoginForm
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+      </>
     );
   } else {
     return (
       <div>
+        <Notification
+          message={notificationMessage}
+          type={notificationType}
+        />
         <h2>blogs</h2>
         <p>
           {user.username} is Logged In.{' '}
           <button onClick={handleLogout}>Logout</button>
         </p>
 
+        {user && (
+          <AddBlogForm
+            title={title}
+            setTitle={setTitle}
+            author={author}
+            setAuthor={setAuthor}
+            url={url}
+            setUrl={setUrl}
+            handleAddBlog={handleAddBlog}
+          />
+        )}
+
         {blogs.map((blog) => (
           <Blog
-            key={blog.id}
+            key={`${blog.id}_${blog.title}`}
             blog={blog}
           />
         ))}
--- part5/blog/frontend/src/components/Notification.js
+++ part5/blog/frontend/src/components/Notification.js
@@ -1,13 +1,16 @@
 const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null;
+  }
   if (type === 'error') {
     return (
-      <div>
+      <div className={type}>
         {type}: {message}
       </div>
     );
   } else if (type === 'success') {
     return (
-      <div>
+      <div className={type}>
         {message} - {type}
       </div>
     );
