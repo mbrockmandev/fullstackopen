41e1fc0cafb7b725127ad90511ffe08e751538bd
Michael Brockman <mbrockmandev@gmail.com>
2023-04-03 14:30:22 -0400

    6a walkthrough upto 6.1

---
+++ part6/reduxtest/src/reducers/noteReducer.js
@@ -0,0 +1,16 @@
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return [...state, action.payload];
+    case 'TOGGLE_IMPORTANCE':
+      const id = action.payload.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      return state.map((n) => (n.id !== id ? n : changedNote));
+    default:
+      return state;
+  }
+};
