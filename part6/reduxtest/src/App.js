7b320bb5f8f7652264ee233530154a412e28f45b
Michael Brockman <mbrockmandev@gmail.com>
2023-04-03 20:19:49 -0400

    start on important filter

--- part6/reduxtest/src/App.js
+++ part6/reduxtest/src/App.js
@@ -1,19 +1,58 @@
-import './App.css';
+import { createNote, toggleImportanceOf } from './reducers/noteReducer';
+import { useSelector, useDispatch } from 'react-redux';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state);
+  const importantNotes = useSelector((state) =>
+    state.filter((n) => n.important),
+  );
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const content = e.target.note.value;
+    e.target.note.value = '';
+    dispatch(createNote(content));
+  };
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id));
+  };
+
+  const toggleShowImportant = (e) => {
+    e.preventDefault();
+    const label = document.querySelector('#label-important');
+    if (e.target.innerText === 'Show Important') {
+      label.innerText = 'Show All';
+      e.target.innerText = 'Show All';
+    } else {
+      label.innerText = 'Show Important';
+      e.target.innerText = 'Show Important';
+    }
+  };
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Add Note:</p>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
+      <div className='container-important'>
+        <p id='label-important'>Show Important</p>
+        <button onClick={toggleShowImportant}>Show Important</button>
+      </div>
+      <ul>
+        {notes.map((note) => (
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
