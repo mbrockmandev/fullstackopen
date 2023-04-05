ff1e54273145edb092fac9ab1f9db08693f62ab3
Michael Brockman <mbrockmandev@gmail.com>
2023-04-04 20:27:09 -0400

    done up to 6.14

--- part6/reduxtest/src/index.js
+++ part6/reduxtest/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import App from './App';
-
 import noteReducer from './reducers/noteReducer';
 import filterReducer from './reducers/filterReducer';
 
@@ -13,8 +12,6 @@ const store = configureStore({
     filter: filterReducer,
   },
 });
-
-console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
