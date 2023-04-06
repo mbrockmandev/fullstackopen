b4cfd405467fedb54beb9b53b147d2a688e07c0a
Michael Brockman <mbrockmandev@gmail.com>
2023-04-06 07:28:06 -0400

    done up to 6.23

--- part6/hook-counter/src/App.js
+++ part6/hook-counter/src/App.js
@@ -1,40 +1,16 @@
-import React from 'react';
-import { useReducer } from 'react';
-import CounterContext from './CounterContext';
-
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
-
-const Button = ({ dispatch, type, label }) => {
-  return <button onClick={() => dispatch({ type })}>{label}</button>;
-};
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-    case 'ZERO':
-      return 0;
-    default:
-      return state;
-  }
-};
+import Button from './components/Button';
+import Display from './components/Display';
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0);
-
   return (
-    <CounterContext.Provider value={[counter, counterDispatch]}>
-      <Display counter={counter} />
+    <div>
+      <Display />
       <div>
-        <Button dispatch={counterDispatch} type={'INC'} label={'+'} />
-        <Button dispatch={counterDispatch} type={'ZERO'} label={'0'} />
-        <Button dispatch={counterDispatch} type={'DEC'} label={'-'} />
+        <Button type={'INC'} label={'+'} />
+        <Button type={'ZERO'} label={'0'} />
+        <Button type={'DEC'} label={'-'} />
       </div>
-    </CounterContext.Provider>
+    </div>
   );
 };
 
--- part6/hook-counter/src/index.js
+++ part6/hook-counter/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CounterContextProvider } from './CounterContext';
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 if (root) {
-  ReactDOM.createRoot(root).render(<App />);
+  root.render(
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>,
+  );
 } else {
-  console.error('Could not find root element.')
+  console.error('Could not find root element.');
 }
