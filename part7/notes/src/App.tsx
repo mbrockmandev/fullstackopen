3c9e3532a8ff296d52ce0f711f61b9185998c334
Michael Brockman <mbrockmandev@gmail.com>
2023-04-09 08:46:29 -0400

    walkthrough up to 7.4

--- part7/notes/src/App.tsx
+++ part7/notes/src/App.tsx
@@ -6,111 +6,113 @@ import Notes from './components/Notes';
 import Users from './components/Users';
 import Login from './components/Login';
 import React from 'react';
+import { useCounter } from './hooks/useCounter';
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      content: 'HTML is easy',
-      important: true,
-      user: 'Matti Luukkainen',
-    },
-    {
-      id: 2,
-      content: 'Browser can execute only JavaScript',
-      important: false,
-      user: 'Matti Luukkainen',
-    },
-    {
-      id: 3,
-      content: 'Most important methods of HTTP-protocol are GET and POST',
-      important: true,
-      user: 'Arto Hellas',
-    },
-  ]);
 
-  const [user, setUser] = useState<string | null>(null);
+    const [notes, setNotes] = useState([
+        {
+            id: 1,
+            content: 'HTML is easy',
+            important: true,
+            user: 'Matti Luukkainen',
+        },
+        {
+            id: 2,
+            content: 'Browser can execute only JavaScript',
+            important: false,
+            user: 'Matti Luukkainen',
+        },
+        {
+            id: 3,
+            content: 'Most important methods of HTTP-protocol are GET and POST',
+            important: true,
+            user: 'Arto Hellas',
+        },
+    ]);
 
-  const login = (user: string) => {
-    setUser(user);
-  };
+    const [user, setUser] = useState<string | null>(null);
 
-  const padding = {
-    padding: 5,
-  };
+    const login = (user: string) => {
+        setUser(user);
+    };
 
-  const match = useMatch('/notes/:id');
-  const note = match
-    ? notes.find((n) => n.id === Number(match.params.id))
-    : null;
+    const padding = {
+        padding: 5,
+    };
 
-  return (
-    <div>
-      <div>
-        <Link
-          style={padding}
-          to='/'>
-          home
-        </Link>
-        <Link
-          style={padding}
-          to='/notes'>
-          notes
-        </Link>
-        <Link
-          style={padding}
-          to='/users'>
-          users
-        </Link>
-        {user ? (
-          <em>{user} logged in</em>
-        ) : (
-          <Link
-            style={padding}
-            to='/login'>
-            login
-          </Link>
-        )}
-      </div>
+    const match = useMatch('/notes/:id');
+    const note = match
+        ? notes.find((n) => n.id === Number(match.params.id))
+        : null;
+    //
+    return (
+        <div>
+            <div>
+                <Link
+                    style={padding}
+                    to='/'>
+                    home
+                </Link>
+                <Link
+                    style={padding}
+                    to='/notes'>
+                    notes
+                </Link>
+                <Link
+                    style={padding}
+                    to='/users'>
+                    users
+                </Link>
+                {user ? (
+                    <em>{user} logged in</em>
+                ) : (
+                    <Link
+                        style={padding}
+                        to='/login'>
+                        login
+                    </Link>
+                )}
+            </div>
 
-      <Routes>
-        <Route
-          path='/notes/:id'
-          element={<Note note={note} />}
-        />
-        <Route
-          path='/notes'
-          element={<Notes notes={notes} />}
-        />
-        <Route
-          path='/users'
-          element={
-            user ? (
-              <Users />
-            ) : (
-              <Navigate
-                replace
-                to='/login'
-              />
-            )
-          }
-        />
-        <Route
-          path='/login'
-          element={<Login onLogin={login} />}
-        />
-        <Route
-          path='/'
-          element={<Home />}
-        />
-      </Routes>
+            <Routes>
+                <Route
+                    path='/notes/:id'
+                    element={<Note note={note} />}
+                />
+                <Route
+                    path='/notes'
+                    element={<Notes notes={notes} />}
+                />
+                <Route
+                    path='/users'
+                    element={
+                        user ? (
+                            <Users />
+                        ) : (
+                            <Navigate
+                                replace
+                                to='/login'
+                            />
+                        )
+                    }
+                />
+                <Route
+                    path='/login'
+                    element={<Login onLogin={login} />}
+                />
+                <Route
+                    path='/'
+                    element={<Home />}
+                />
+            </Routes>
 
-      <footer>
-        <br />
-        <em>Note app, Department of Computer Science 2023</em>
-      </footer>
-    </div>
-  );
+            <footer>
+                <br />
+                <em>Note app, Department of Computer Science 2023</em>
+            </footer>
+        </div>
+    );
 };
 
 export default App;
