2fb9d0e0e6be55c0898530abc40cd26f5e187ea5
Michael Brockman <mbrockmandev@gmail.com>
2023-04-11 16:00:25 -0400

    add 8.1-8.2

--- part8/index.js
+++ part8/index.js
@@ -39,9 +39,14 @@ type Person {
   id: ID!
 } 
 
+enum YesNo {
+  YES
+  NO
+}
+
 type Query {
   personCount: Int!
-  allPersons: [Person!]!
+  allPersons(phone: YesNo): [Person!]!
   findPerson(name: String!): Person
 }
 
@@ -52,13 +57,23 @@ type Mutation {
     street: String!
     city: String!
   ): Person
-}
-`;
+  editNumber(
+    name: String!
+    phone: String!
+  ): Person
+}`;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons;
+      }
+      const byPhone = (person) =>
+        args.phone === "YES" ? person.phone : !person.phone;
+      return persons.filter(byPhone);
+    },
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
   Person: {
@@ -83,6 +98,16 @@ const resolvers = {
       const person = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
+    },
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name);
+      if (!person) {
+        return null;
+      }
+
+      const updatedPeron = { ...person, phone: args.phone };
+      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
+      return updatedPeron;
     },
   },
 };
