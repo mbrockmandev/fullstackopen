99ceae0ea81e853d419f54b090ab8678a9db9cdf
Michael Brockman <mbrockmandev@gmail.com>
2023-04-12 07:10:57 -0400

    8-1 done

---
+++ part8/graphql-fe/src/Person.jsx
@@ -0,0 +1,30 @@
+import { gql } from "@apollo/client";
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
+
+const Person = ({ person, onClose }) => {
+  return (
+    <div>
+      <h2>{person.name}</h2>
+      <div>
+        {person.address.street} {person.address.city}
+      </div>
+      <div>{person.phone}</div>
+      <button onClick={onClose}>close</button>
+    </div>
+  );
+};
+
+export default Person;
